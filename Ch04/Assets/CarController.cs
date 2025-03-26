94c3f8bffc2aef6d51598304a8b4b51c67c4a063
kjynss <101390983+kjynss@users.noreply.github.com>
2025-03-26 16:18:56 +0900

    20250326

---
+++ Ch04/Assets/CarController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    float stopSpeed = 0.01f;
+    float decreaseRate = 0.98f;
+    float speedRatio = 0.0004f;
+
+    Vector2 startPos;
+    Vector2 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+            speed = swipeLength * speedRatio;
+        }
+
+        transform.position += new Vector3(speed, 0, 0);
+        speed *= decreaseRate;
+
+        if(speed < stopSpeed)
+        {
+            speed = 0;
+        }
+    }
+}
---
+++ Ch04/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject flag;
+    public GameObject distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //car = GameObject.Find("car");
+        //flag = GameObject.Find("flag");
+        //distance = GameObject.Find("distance");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (car == null) return;
+        if (flag == null) return;
+        if (distance == null) return;
+        float length = flag.transform.position.x - car.transform.position.x;
+        distance.GetComponent<TextMeshProUGUI>().text = "Distance: " + length.ToString("F2") + "m";
+    }
+}
